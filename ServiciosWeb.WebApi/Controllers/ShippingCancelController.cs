82df2a8a493da7c959bbcc369370d5cb3ac4f062
Carlos Fausto Huaman Rengifo <carlos.huaman@grupomok.com>
2020-05-28 13:32:13 -0500

    Se agrega controlador de PickupNew

---
+++ ServiciosWeb.WebApi/Controllers/ShippingCancelController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using ServiciosWeb.Datos.Modelo;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using ServiciosWeb.Datos;
+
+namespace ServiciosWeb.WebApi.Controllers
+{
+    public class ShippingCancelController : ApiController
+    {
+        private LibreriaConnection dbContext = new LibreriaConnection();
+
+        [HttpGet]
+        public IEnumerable<RootObject> Get()
+        {
+            var listado = dbContext.RootObject.ToList();
+            return listado;
+        }
+
+        [HttpGet]
+        public RootObject Get(string id)
+        {
+            var rootObject = dbContext.RootObject.FirstOrDefault(x => x.message_id == id);
+            return rootObject;
+        }
+
+        [HttpPost]
+
+        public IHttpActionResult AgregaTicket([FromBody] RootObject root)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.RootObject.Add(root);
+                dbContext.SaveChanges();
+                return Ok(root);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        
+
+
+    }
+}
